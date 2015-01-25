624395ee4f721182ede364df1db13938fff48fcf
zaphod <whitep@vcu.edu>
2015-01-25 16:50:00 -0500

    changed repo structure for better branching

--- connect.rb
+++ connect.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/env ruby
 # connect.rb
-# make connection to chat.smartbrief.com
+# Module to handle connections made between bot
+# and SB jabber server
 # By: Patrick White
 
-require 'xmpp4r'
-require 'xmpp4r/framework/bot'
 
-client = new.Client
+=begin
+  This module will serve as a connection proxy
+  for all the bot's functions. Should try attaching
+  to globals.conf for username and password in
+  an effort to keep credentials out of this project
 
-
-def connection
-  # make connection to smartbrief chat server
-end
+  Needs to:
+    - Make connection
+    - Destroy connection
+    - Check connection
+    - Load users/chat rooms
+    - Kill / Restart commands
+=end
